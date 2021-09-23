1a3e185113d2a36f5dd04cab5d9dcef234df3ec0
durgesh muley <durgeshmuley@rediffmail.com>
2021-09-23 20:45:34 +0530

    frontend files

---
+++ client/build/precache-manifest.110449e2ca4a57985ca64358d045b6e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c878cdb3183ed26e24f90d2a7ee78c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1bd0adcaf83b94fdb00",
+    "url": "/static/css/main.11e78aa1.chunk.css"
+  },
+  {
+    "revision": "acc20f325322bd2ec5ab",
+    "url": "/static/js/2.0c4707c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c4707c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bd0adcaf83b94fdb00",
+    "url": "/static/js/main.58b872cf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "a5d77d2bfe24b1d93527de83bb165d3e",
+    "url": "/static/media/fclogo2.a5d77d2b.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
